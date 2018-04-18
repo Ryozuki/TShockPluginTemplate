66b085111838e04e99c71a8cfb92a772b57ebaa0
Ryozuki <edgar@ryobyte.com>
2018-04-18 17:17:34 +0200

    s

--- TshockPlugin/Class1.cs
+++ TshockPlugin/Class1.cs
@@ -1,11 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
-$endif$using System.Text;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
 
 namespace $safeprojectname$
 {
-	public class Class1
-{
-}
+	[ApiVersion(2, 1)]
+    public class $safeprojectname$ : TerrariaPlugin
+    {
+        #region Plugin Info
+        public override string Name => "$safeprojectname$";
+        public override string Author => "Ryozuki";
+        public override string Description => "Replace me!!!";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        #endregion
+        
+        public ConfigFile Config = new ConfigFile();
+
+        public $safeprojectname$(Main game) : base(game)
+        {
+        }
+
+        private void LoadConfig()
+        {
+            string path = Path.Combine(TShock.SavePath, "$safeprojectname$.json");
+            Config = ConfigFile.Read(path);
+        }
+
+        public override void Initialize()
+        {
+            LoadConfig();
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+            ServerApi.Hooks.GamePostInitialize.Register(this, OnPostInitialize);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+                ServerApi.Hooks.GamePostInitialize.Deregister(this, OnPostInitialize);
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Hooks
+        private void OnInitialize(EventArgs args)
+        {
+            DB.Connect();
+            Commands.ChatCommands.Add(new Command("$safeprojectname$.help".ToLower(), CHelp, "chelp")
+            {
+                HelpText = "Usage: /chelp"
+            });
+        }
+
+        private void OnPostInitialize(EventArgs args)
+        {
+
+        }
+        #endregion
+
+        #region Commands
+        private void CHelp(CommandArgs args)
+        {
+            args.Player.SendInfoMessage("Author, please change me.");
+        }
+        #endregion
+    }
 }
---
+++ TshockPlugin/ConfigFile.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace $safeprojectname$
+{
+    public class ConfigFile
+    {
+        #region ConfigVars
+
+        public string PluginName = "$safeprojectname$";
+        public bool UseMySQL = false;
+
+        #endregion ConfigVars
+
+        public static ConfigFile Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                ConfigFile config = new ConfigFile();
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+                return config;
+            }
+            return JsonConvert.DeserializeObject<ConfigFile>(File.ReadAllText(path));
+        }
+    }
+}
